036cb2dc513f0c0090fbbbbf8b60bdd02f27e10b
Bhargav Marada <bmarada@users.campus>
2020-09-27 18:42:36 -0500

    progress

---
+++ PointOfSale/AllItems.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for AllItems.xaml
+    /// </summary>
+    public partial class AllItems : UserControl
+    {
+        public AllItems()
+        {
+            InitializeComponent();
+        }
+
+        private void BriarheartBurger_Click(object sender, RoutedEventArgs e)
+        {
+            var Control = this.FindAncestor<Control>();
+            Control.ChangeScreen(new BriarheartBurger());
+        }
+
+    }
+
+   
+}
---
+++ PointOfSale/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PointOfSale
+{
+    public static class Extensions
+    {
+        /// <param name="obj">The first ancestor of type T, or null</param>
+        /// <returns></returns>
+        public static T FindAncestor<T>(this DependencyObject obj) where T : DependencyObject
+        {
+
+            var parent = VisualTreeHelper.GetParent(obj);
+
+            if (parent == null) return null;
+
+            if (parent is T) return parent as T;
+
+            return FindAncestor<T>(parent);
+
+        }
+    }
+}
