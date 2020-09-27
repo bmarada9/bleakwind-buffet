d1dd70bb323f018e215ee283991a5e13e639beae
Bhargav Marada <bmarada@users.campus>
2020-09-27 17:11:09 -0500

    Grinding

--- PointOfSale/BriarheartBurger.xaml.cs
+++ PointOfSale/BriarheartBurger.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,16 +14,13 @@ using System.Windows.Shapes;
 namespace PointOfSale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BriarheartBurger.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BriarheartBurger : UserControl
     {
-        public MainWindow()
+        public BriarheartBurger()
         {
             InitializeComponent();
-            
         }
-
-        
     }
 }
